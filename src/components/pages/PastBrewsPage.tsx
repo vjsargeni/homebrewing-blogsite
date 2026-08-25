ffd48eb6b186edad3eab307ac7a2ed318e7688d1
vjsargeni <vsargeni@udel.edu>
2026-08-24 20:01:57 -0400

    expanding fetch

--- src/components/pages/PastBrewsPage.tsx
+++ src/components/pages/PastBrewsPage.tsx
@@ -11,7 +11,7 @@ interface PastBrewsPageProps {}
 const PastBrewsPage: FC<PastBrewsPageProps> = async (props) => {
   const query: Where = { brewingStatus: { equals: 'past' } }
 
-  const data = await GetBrewsFromPayloadByCondition(query, 10)
+  const data = await GetBrewsFromPayloadByCondition(query, 100)
   const pastBrews = data.filter((brew) => brew.brewingStatus === BREWING_STATUS.PAST)
 
   //filter/search?
